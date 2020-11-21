23c1a7843b531355faab9e5587bf5dd08cb7ce1f
khalidino <khaliditman@gmail.com>
2020-11-21 14:11:39 +0300

    Created E-Commerse-Site-Online Application on Vue JS

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="Navbar">
+    <h1> Navbar </h1>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+}
+</script>
+
+<style scoped>
+
+</style>
